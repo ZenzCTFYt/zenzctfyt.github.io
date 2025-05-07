3a402e1bc28b5fda0aa362420de86ff76e1a263a
LutfitkHensem <zenzctf@gmail.com>
2025-05-07 18:32:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
+// Efek mengetik untuk elemen .code
+const codeElement = document.querySelector('.code');
+const codeText = codeElement.innerHTML;
+codeElement.innerHTML = '';
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    burger.classList.toggle('toggle');
-});
+let i = 0;
+function typeEffect() {
+    if (i < codeText.length) {
+        codeElement.innerHTML += codeText.charAt(i);
+        i++;
+        setTimeout(typeEffect, 100);
+    }
+}
+
+window.onload = typeEffect;
